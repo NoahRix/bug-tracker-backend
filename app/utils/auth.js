b8d0b0cd6b354ad500307316f20d108d1793a4f0
unknown <rix.noah@yahoo.com>
2021-03-05 14:08:32 -0600

    updated cors

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -6,7 +6,6 @@ let debug = true;
 
 exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    res.header('Access-Control-Allow-Origin', '*');
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
