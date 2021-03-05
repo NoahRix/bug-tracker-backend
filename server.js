f474de19fbdfd13cb9d43c2590c30dc6a72783da
unknown <rix.noah@yahoo.com>
2021-03-05 14:11:45 -0600

    updated cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const rt = require('./app/utils/RouterTools');
 const { response } = require('express');
 
 app.use(express.json());
@@ -11,11 +12,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-const rt = require('./app/utils/RouterTools');
-
-console.log("Bug Tracker backend Starting...");
+app.options('*', cors()); 
 
 //Cross Origins configuration.
 //var whitelist = ['http://localhost:3000', 'http://localhost:3001', '69.174.145.39:3000'];
