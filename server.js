d447257de5d0fef75689e739abbbad1923c4def8
unknown <rix.noah@yahoo.com>
2021-03-05 13:34:00 -0600

    updated cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.options('*', cors());
 
 const rt = require('./app/utils/RouterTools');
 
